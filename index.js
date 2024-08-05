a50e13a8e42c7ce78f04a443af52598fd794d100
Kauan Bobrowic <34657955-kauanbobrowic@users.noreply.replit.com>
2024-08-05 22:55:49 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const meuNome = "Martinez";
+const idade = 17;
+const altura = 1.82
+const massa = 68;
+const cidade = "São José dos Pinhais"
+
+console.log(altura)
+console.log(massa)
+console.log(cidade)
+console.log(meuNome)
+console.log(idade)
